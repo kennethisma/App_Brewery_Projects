ae9b1b6a6a6d450241460831c64b589d5a4d9794
Kenneth Loaisiga <iloaisigasanchez@gmail.com>
2021-05-13 18:58:19 -0600

    Higher and lower proyect

--- Day14/Higher_lower_game.py
+++ Day14/Higher_lower_game.py
@@ -1,70 +1,79 @@
-# import random and game data
-import random
 from game_data import data
-# get two random famous from the list game data.
+import random
+from art import logo, vs
+from replit import clear
 
 
-def random_dict():
-    '''Returns a random famous from game_data'''
-    lenght_data = len(data)
-    random_number = random.randint(0, lenght_data)
-    random_famed = data[random_number - 1]
-    return random_famed
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
 
 
-def getting_followers(chose):
-    '''returns followers'''
-    if chose == 'a':
-        return followers_a
-    elif chose == 'b':
-        return followers_b
+def format_data(account):
+    """Format account into printable format: name, description and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    # print(f'{name}: {account["follower_count"]}')
+    return f"{name}, a {description}, from {country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess 
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
     else:
-        return
+        return guess == "b"
 
 
-def compare_followers(famous_chose, score):
-    if famous_chose == followers_a:
-        if famous_chose > followers_b:
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        clear()
+        print(logo)
+        if is_correct:
             score += 1
-            print(f"You're right! Current score: {score}")
-            return score
-        elif famous_chose < followers_b:
-            print(f"You're wrong! Final score: {score}")
-            return score
+            print(f"You're right! Current score: {score}.")
         else:
-            print(f"You're wrong! Final score: {score}")
-            return score
-    elif famous_chose == followers_b:
-        if famous_chose > followers_a:
-            score += 1
-            print(f"You're right! Current score: {score}")
-            return score
-        elif famous_chose < followers_b:
-            print(f"You're wrong! Final score: {score}")
-            return score
-        else:
-            print(f"You're wrong! Final score: {score}")
-            return score
-    else:
-        print(f"You're wrong! Final score: {score}")
-        return score
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
 
 
-famed_a = random_dict()
-famed_b = random_dict()
-followers_a = famed_a['follower_count']
-followers_b = famed_b['follower_count']
-score = 0
-is_game_over = False
+game()
 
-print(
-    f"Compare A: {famed_a['name']}, a {famed_a['description']}, from {famed_a['country']}")
-print(
-    f"Against B: {famed_b['name']}, a {famed_b['description']}, from {famed_b['country']}")
 
-chose = input("Who has more followers? Type 'A' or 'B': ").lower()
+# Check if user is correct.
+# Get follower count of each account
+# use if statement to check if user is correct
 
-famous_chose = getting_followers(chose)
+# Give user feedback on their guess
 
-score = compare_followers(famous_chose, score)
-print(score)
+# score keeping
+
+# Make the game repeatable.
+
+# making account at position B become the next account at position A
+
+# clear the screen between rounds.
